7ae04658978c799193a69c0da82a69dcada18f23
wudesai <3058856057@qq.com>
2020-02-04 15:37:07 +0800

    second commit project

--- src/test/java/com/imooc/sell/service/impl/CategoryServiceImplTest.java
+++ src/test/java/com/imooc/sell/service/impl/CategoryServiceImplTest.java
@@ -11,6 +11,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 测试
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CategoryServiceImplTest {
